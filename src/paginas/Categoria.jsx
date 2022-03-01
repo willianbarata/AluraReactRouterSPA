2b3eb193826f8d568d5b17b7f90124dbc175f62f
willianbarata <willianbarata@gmail.com>
2022-03-01 10:46:20 -0300

    Aula 12 - Criando o Categoria

---
+++ src/paginas/Categoria.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/css/blog.css';
+
+const Categoria = () => {
+    return(
+        <>
+        <div className="container">
+            <h2 className="titulo-pagina">Pet Notícias</h2>
+        </div>
+        </>
+    )
+}
+
+export default Categoria;
