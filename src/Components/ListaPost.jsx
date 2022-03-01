1903fcd2437e2f361dabaa1418315f6cd90af71b
willianbarata <willianbarata@gmail.com>
2022-03-01 09:49:30 -0300

    Aula 7 - Criando conexão com API

---
+++ src/Components/ListaPost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { busca } from "../api/api";
+
+const ListaPost = () => {
+    return(
+        <section className="post container">
+
+        </section>
+    );
+}
+
+export default ListaPost;
