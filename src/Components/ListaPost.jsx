4873dc50bef7a846680ac84c638b26799f320cfb
willianbarata <willianbarata@gmail.com>
2022-03-01 10:11:33 -0300

    Aula 8 - Listando dados da API

--- src/Components/ListaPost.jsx
+++ src/Components/ListaPost.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { busca } from "../api/api";
+import { Link } from "react-router-dom";
 
-const ListaPost = () => {
+const ListaPost = ({url}) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        busca(url, setPosts)    
+    }, [])
+
+    
     return(
-        <section className="post container">
-
+        <section className="posts container">
+            { 
+            posts.map((post)=>{
+              return(  <Link className={`cartao-post cartao-post--${post.categoria}`} to="">
+                    <article key={post.id}>
+                        <h3 className="cartao-post__titulo">
+                            {post.title}
+                        </h3>
+                        <p className="cartao-post__meta"> {post.metadescription}</p>
+                    </article>
+                </Link>
+            );
+            })
+            }
         </section>
     );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export const api = axios.create({
     baseURL: 'http://localhost:5000'
 });
 
-export const busca = async () =>{
+export const busca = async (url, setDado) =>{
     const resposta = await api.get(url);
-    setDado(resposta.data)
+    setDado(resposta.data);
 }
