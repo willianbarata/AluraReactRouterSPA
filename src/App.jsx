d07c990ed1e7186ca3f9a73daf54f83494420f83
willianbarata <willianbarata@gmail.com>
2022-03-01 08:30:03 -0300

    Aula 1 - Criando primeira rota

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './assets/css/base/base.css';
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+
+function App() {
+  const Router = ()=> {
+    const location = window.location.pathname
+    if(location === '/sobre'){
+      return <Sobre/>
+    }else {
+      return <Home/>
+    }
+  }
+  return (
+  <>
+      {Router()}
+  </>
+  )
+}
+
+export default App
