ddf12f8ba7876e5513a5cb6c48210741d4ccb376
willianbarata <willianbarata@gmail.com>
2022-03-01 11:38:55 -0300

    Aula 15 - Listando subcategorias dinamicamente

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,31 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { Route, useParams, useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import ListaCategorias from '../Components/ListaCategorias'
+import ListaPost from '../Components/ListaPost'
+import '../assets/css/blog.css'
+import { Route, useParams, useRouteMatch, Link, Switch } from 'react-router-dom';
 import { busca } from '../api/api';
-import '../assets/css/blog.css';
-import ListaCategorias from '../Components/ListaCategorias';
-import ListaPost from '../Components/ListaPost';
+import SubCategoria from '../paginas/SubCategoria';
+
 
 const Categoria = () => {
-    const { id } = useParams();
-    const { path } = useRouteMatch();
-    const [ subCategorias, setSubCategorias ] = useState([]);
-    
-    useEffect(()=>{
-        busca(`/categorias/${id}`, (categoria)=>{
-            setSubCategorias(categoria.subCategorias)
-        })
-    }, [])
-    return(
-        <>
-        <div className="container">
-            <h2 className="titulo-pagina">Pet Notícias</h2>
-        </div>
-        <ListaCategorias/>
-        <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-        </Route>
-        </>
-    )
-}
+  const { id } = useParams()
+  const { url, path } = useRouteMatch()
+  const [subcategorias, setSubCategorias] = useState([])
 
-export default Categoria;
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias)
+    })
+  }, [id])
+  return (
+    <>
+      <div className="container">
+        <h2 className="titulo-pagina">Pet Notícias</h2>
+      </div>
+
+      <ListaCategorias />
+      <ul className="lista-categorias container flex">
+        {
+          subcategorias.map((subcategoria) => (
+            <li
+              className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+              key={subcategoria}
+            >
+              <Link to={`${url}/${subcategoria}`}>
+                {subcategoria}
+              </Link>
+            </li>
+          ))
+
+        }
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
+    </>
+  )
+}
+export default Categoria
--- src/paginas/SubCategoria.jsx
+++ src/paginas/SubCategoria.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import ListaPost from "../Components/ListaPost";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import ListaPost from '../Components/ListaPost';
 
-const SubCategoria = () =>{
-    const { subcategoria } = useParams();
-    
-    return(
-        <ListaPost url={`/post?subcategoria=${subcategoria}`} />
-    )
+const SubCategoria = () => { 
+  const { subcategoria } = useParams();
+  return(
+    <ListaPost url={`/posts?subcategoria=${subcategoria}`} />
+  )
 }
 
 export default SubCategoria;
