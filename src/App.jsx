4a73e817d0080a45e1aa05af77212a5bd0002e5d
willianbarata <willianbarata@gmail.com>
2022-03-01 08:42:35 -0300

    Aula 3 - aplicando BrowserRouter - Route - Switch

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import './assets/css/base/base.css';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 
 function App() {
   
   return (
  <Router>
-   <Route>
-      <Home path='/'/>
+   <Switch>
+   <Route exact path='/'>
+      <Home />
    </Route>
-   <Route>
-      <Sobre path='/sobre'/> 
+   <Route path='/sobre'>
+      <Sobre /> 
    </Route>
+   </Switch>
  </Router>
   )
 }
